c918859432af8afd3cc2fce08fa07bec9aa4ca35
Fake Smile <64054240+Ivan-ID@users.noreply.github.com>
2020-04-21 11:56:17 +0700

    Create Sub.py

---
+++ jangan-di-buka/Sub.py
@@ -0,0 +1,14 @@
+from requests import ConnectionError
+from time import sleep
+import requests, sys, os, re, random
+
+def Spam():
+        os.system('clear')
+        sleep(1)
+        os.system('xdg-open https://www.youtube.com/channel/UCzsADl8XRJeZXJ6CKZLX5KQ')
+        os.system('clear')
+        sleep(1)
+        print
+
+if __name__ == '__main__':
+        Spam()
